36ed428e7293287e368cd9321c8e9c1bcd4b0eeb
Anik Ghosh <anikghosh46.bd@gmail.com>
2023-03-04 16:37:32 +0600

    Functions helper added.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const functions = require('./helpers/functions')
 
 
 /**
@@ -12,16 +13,29 @@ function compile(filePath, variables = {}) {
     // Read the content of the file
     const content = fs.readFileSync(filePath, 'utf8')
 
-    // Define the regular expression pattern for variables
-    const pattern = /\$\{(\w+)\}/g
+    // Regex pattern for variables. It can be ${name} or ${f(name)}
+    const pattern = /\$\{(\w+|\w+\(\w+\))\}/g
   
     // Replace all variables in the content with their corresponding values
     const replacedContent = content.replace(pattern, (match, variable) => {
+
+      // check if has a function in pattern
+      const functionMatch = variable.match(/\w+\((\w+)\)/)
+      if (functionMatch) {
+        const functionName = variable.split('(')[0]
+        const functionArg = functionMatch[1]
+        const functionArgValue = variables[functionArg]
+
+        if (functions.hasOwnProperty(functionName) && functionArgValue) {
+          return functions[functionName](functionArgValue)
+        }
+      }
+
       if (variables.hasOwnProperty(variable)) {
         return variables[variable]
-      } else {
-        return match
       }
+
+      return match
     })
   
     return replacedContent
