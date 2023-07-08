1e48a3375e38189872cbd9dc5443630c3faaa8b6
Anik Ghosh <52236930+anikghosh256@users.noreply.github.com>
2023-07-08 10:14:22 +0600

    Capitalize and pluralize added. (#6)
    New functions: 
    - `pluralize(string)` - Change the word to its plural
    - `singularize(string)` - Change the word to its singular
    - `capPluralize(string)` - capitalize + pluralize
    - `capSingularize(string)` - capitalize + singularize

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,3 +1,5 @@
+const pluralize = require('pluralize')
+
 /**
  * String helpers
  */
@@ -15,4 +17,26 @@ exports.lowercase = function (str) {
 // capitalize
 exports.capitalize = function (str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+//pluralize
+exports.pluralize = function (str) {
+  return pluralize.plural(str)
+}
+
+//capitalize + pluralize
+exports.capPluralize = function (str) {
+  str = exports.capitalize(str)
+  return pluralize.plural(str)
+}
+
+//singularize
+exports.singularize = function (str) {
+  return pluralize.singular(str)
+}
+
+//capitalize + singularize
+exports.capSingularize = function (str) {
+  str = exports.capitalize(str)
+  return pluralize.singular(str)
 }
--- test/main.test.js
+++ test/main.test.js
@@ -6,7 +6,7 @@ describe('compile', () => {
   const filePath = join(__dirname, 'template.ct')
   const expectedFilePath = join(__dirname, 'expected.ct')
   it('should compile', () => {
-    const result = compile(filePath, { name: 'compiled' }).trim()
+    const result = compile(filePath, { name: 'compiled', book: "book", books: "books" }).trim()
     const receivedString = fs.readFileSync(expectedFilePath, 'utf8').trim()
 
     expect(result).toMatch(receivedString)
